aa4e75a3a0edaff0f8cb7e6d381f93e0e34ac2c8
Eric <rumperic@gmail.com>
2024-02-28 11:27:52 +0100

    aufgabe 3 gelöst

--- 03-filter-api-data/filter-api-data.js
+++ 03-filter-api-data/filter-api-data.js
@@ -1,3 +1,20 @@
 function filterApiData(apiData, mandatoryKeys) {
-  // Add you solution here
+  let result = [];
+  for (let i = 0; i < apiData.length; i++) {
+    let daten = apiData[i];
+
+    let allesda = true;
+
+    for (let j = 0; j < mandatoryKeys.length; j++) {
+      let key = mandatoryKeys[j];
+      if (!daten.hasOwnProperty(key)) {
+        allesda = false;
+        break;
+      }
+    }
+    if (allesda) {
+      result.push(daten);
+    }
+  }
+  return result;
 }

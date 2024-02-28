4d35fa12af4a10335725317e61c2c435b4dd8fff
Eric <rumperic@gmail.com>
2024-02-28 11:48:22 +0100

    aufgabe 1 gelöst

--- 01-zip-strings/zip-strings.js
+++ 01-zip-strings/zip-strings.js
@@ -1,3 +1,17 @@
 function zipStrings(strA, strB) {
-  // Your Code here
+  let A = strA.split("");
+  let B = strB.split("");
+  let merged = [];
+  const lang = Math.max(A.length, B.length);
+
+  for (let i = 0; i < lang; i++) {
+    if (i < A.length) {
+      merged.push(A[i]);
+    }
+    if (i < B.length) {
+      merged.push(B[i]);
+    }
+  }
+  let result = merged.join("");
+  return result;
 }
